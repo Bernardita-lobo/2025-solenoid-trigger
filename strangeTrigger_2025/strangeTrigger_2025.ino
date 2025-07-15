63b37e9592ff03ea1306d98fa156de3a1216c6b8
aarón montoya-moraga <montoyamoraga@gmail.com>
2025-07-15 16:22:28 -0400

    agregar comentarios

--- strangeTrigger_2025/strangeTrigger_2025.ino
+++ strangeTrigger_2025/strangeTrigger_2025.ino
@@ -4,6 +4,23 @@
 // siguientes versiones
 // por @misaaaaaa y @montoyamoraga
 
+// para teensy 4.0
+
+// calibrar el ancho del tiempo de activacion del trigger
+// segun velocidad de la nota midi
+
+// hay 3 notas midi, una por woodblock
+// podemos incluso hacerlo con siete
+
+// input: nota midi con velocidad midi
+// procesamos
+// out[]
+
+// variables de longitud de pulso minimo y maximo
+
+// operador modulo %12 para llevarlo a escala 0-11
+// do, re, mi, fa, sol, la, si
+
 #define channels 8
 
 byte nota = 0;
