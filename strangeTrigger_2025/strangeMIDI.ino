9bcf34bada7cb27319b709949c3a42ac032d48e3
montoyamoraga <montoyamoraga@gmail.com>
2025-08-06 18:42:53 -0400

    mover mas codigo entre pestanas

--- strangeTrigger_2025/strangeMIDI.ino
+++ strangeTrigger_2025/strangeMIDI.ino
@@ -4,6 +4,24 @@ void setupMIDI() {
   usbMIDI.setHandleControlChange(myControlChange);
 }
 
+void loopMIDI() {
+  usbMIDI.read();
+  for (int i = 0; i < channels - 2; i++) {
+    // if (nota == notas[i] || nota == notas[i] + 12 || nota == notas[i] + 24 || nota == notas[i] + 36 || nota == notas[i] + 48 || nota == notas[i] + 60 || nota == notas[i] + 72 || nota == notas[i] + 84 || nota == notas[i] + 96) {
+    if (nota == notas[i] % 12) {
+      analogWrite(outPin[i], vel);
+    }
+  }
+
+
+  for (int i = 6; i < channels; i++) {
+    // if (nota == notas[i] || nota == notas[i] + 12 || nota == notas[i] + 24 || nota == notas[i] + 36 || nota == notas[i] + 48 || nota == notas[i] + 60 || nota == notas[i] + 72 || nota == notas[i] + 84 || nota == notas[i] + 96) {
+    if (nota == notas[i] % 12) {
+      digitalWrite(outPin[i], vel);
+    }
+  }
+}
+
 void myControlChange(byte channel, byte control, byte value) {
   Serial.print("Control Change, ch=");
   Serial.print(channel, DEC);
--- strangeTrigger_2025/strangeTrigger_2025.ino
+++ strangeTrigger_2025/strangeTrigger_2025.ino
@@ -1,5 +1,6 @@
 #define channels 8
 
+
 byte nota = 0;
 byte canal = 1;
 byte vel = 0;
@@ -8,7 +9,8 @@ byte vel = 0;
 byte outPin[] = { 3, 4, 5, 6, 9, 10, 15, 16 };
 
 //NOTAS RECONOCIBLES POR MIDI EN LA OCTAVA 0 (C0-G0)
-byte notas[] = { 24, 25, 26, 27, 28, 29, 30, 31 };
+// do, re, mi, fa, sol, la, si
+byte notas[] = { 24, 26, 28, 29, 31, 33, 35 };
 
 elapsedMillis blinking;
 
@@ -31,23 +33,11 @@ void setup() {
 
 // loop() ocurre despues de setup(), en bucle
 void loop() {
+
   loopLedInterno();
 
-  usbMIDI.read();
-  for (int i = 0; i < channels - 2; i++) {
-    // if (nota == notas[i] || nota == notas[i] + 12 || nota == notas[i] + 24 || nota == notas[i] + 36 || nota == notas[i] + 48 || nota == notas[i] + 60 || nota == notas[i] + 72 || nota == notas[i] + 84 || nota == notas[i] + 96) {
-    if (nota == notas[i] % 12) {
-      analogWrite(outPin[i], vel);
-    }
-  }
-
-
-  for (int i = 6; i < channels; i++) {
-    // if (nota == notas[i] || nota == notas[i] + 12 || nota == notas[i] + 24 || nota == notas[i] + 36 || nota == notas[i] + 48 || nota == notas[i] + 60 || nota == notas[i] + 72 || nota == notas[i] + 84 || nota == notas[i] + 96) {
-    if (nota == notas[i] % 12) {
-      digitalWrite(outPin[i], vel);
-    }
-  }
+  loopMIDI();
+  
   Serial.println(nota);
 }
 
